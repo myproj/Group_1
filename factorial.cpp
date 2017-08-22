c377718cbf0233745cdb736189e0c9c05959c82f
pranav_gupta <pranavgupta4321@gmail.com>
2017-08-22 16:29:41 +0530

    case for negative number handled

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,16 @@
 #include "functions.h"
 
 int factorial(int n){
-    if(n!=1){
-	return(n * factorial(n-1));
-    }
-    else return 1;
+
+	if(n < 0){
+		int t;
+		cin>>t;
+		return factorial(t);
+	}else{
+		if(n!=1){
+			return(n * factorial(n-1));
+		}
+		else return 1;	
+	}
+
 }
